1b6a8da06fb6aeeb419ddd24780b23068f53f409
alexgrebenshchikov <qazwsx13240987@gmail.com>
2022-09-30 20:41:40 +0300

    fix mockapi

--- app/src/main/java/com/mcs/emkn/network/MockApi.kt
+++ app/src/main/java/com/mcs/emkn/network/MockApi.kt
@@ -7,27 +7,27 @@ import com.mcs.emkn.network.dto.response.ResponseWithTokenAndTimeDto
 import com.mcs.emkn.network.dto.response.ResponseWithTokenDto
 
 class MockApi: Api {
-    override suspend fun accountsRegister(request: RegistrationRequestDto): NetworkResponse<ResponseWithTokenAndTimeDto, RegistrationErrorResponseErrorsDto> {
+    override suspend fun accountsRegister(request: RegistrationRequestDto): NetworkResponse<ResponseWithTokenAndTimeDto, RegistrationErrorResponseDto> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun validateEmail(request: ValidateEmailRequestDto): NetworkResponse<Unit, ValidateEmailErrorResponseErrorsDto> {
+    override suspend fun validateEmail(request: ValidateEmailRequestDto): NetworkResponse<Unit, ValidateEmailErrorResponseDto> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun accountsLogin(request: LoginRequestDto): NetworkResponse<Unit, LoginErrorResponseErrorsDto> {
+    override suspend fun accountsLogin(request: LoginRequestDto): NetworkResponse<Unit, LoginErrorResponseDto> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun accountsBeginChangePassword(request: BeginChangePasswordRequestDto): NetworkResponse<ResponseWithTokenAndTimeDto, BeginChangePasswordErrorResponseErrorsDto> {
+    override suspend fun accountsBeginChangePassword(request: BeginChangePasswordRequestDto): NetworkResponse<ResponseWithTokenAndTimeDto, BeginChangePasswordErrorResponseDto> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun accountsValidateChangePassword(request: ValidateChangePasswordRequestDto): NetworkResponse<ResponseWithTokenDto, ValidateChangePasswordErrorResponseErrorsDto> {
+    override suspend fun accountsValidateChangePassword(request: ValidateChangePasswordRequestDto): NetworkResponse<ResponseWithTokenDto, ValidateChangePasswordErrorResponseDto> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun accountsCommitChangePassword(request: CommitChangePasswordRequestDto): NetworkResponse<Unit, CommitChangePasswordErrorResponseErrorsDto> {
+    override suspend fun accountsCommitChangePassword(request: CommitChangePasswordRequestDto): NetworkResponse<Unit, CommitChangePasswordErrorResponseDto> {
         TODO("Not yet implemented")
     }
 
